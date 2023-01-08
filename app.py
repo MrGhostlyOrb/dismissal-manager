d0f2c86f20a73680f704ac907b41f5d1bfd3cc0d
Ben Cooper <ben.f.cooper@gmail.com>
2023-01-08 11:07:26 +0000

    FIX: Added extra permissions work and design switching

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ app = Flask(__name__)
 
 app.secret_key = os.environ.get('SECRET_KEY') or SECRET_KEY
 
+design = "xp"
+
 
 def get_all_students():
     # Read students from JSON file
@@ -23,25 +25,25 @@ def get_all_students():
 
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('login.html', design=design)
 
 
 @app.route('/admin')
 def admin():
-    #  check if logged in
-    if not session.get('logged_in'):
+    #  check if logged in and has admin role in list of roles
+    if not session.get('logged_in') or 'admin' not in session.get('roles'):
         return redirect('/')
     students = get_all_students()
-    return render_template('admin.html', students=students)
+    return render_template('admin.html', students=students, design=design)
 
 
 @app.route('/teacher')
 def teacher():
-    #  check if logged in
-    if not session.get('logged_in'):
+    #  check if logged in and has teacher role
+    if not session.get('logged_in') or 'teacher' not in session.get('roles'):
         return redirect('/')
     all_students = get_all_students()
-    return render_template("teacher.html", students=all_students, selected_class="all")
+    return render_template("teacher.html", students=all_students, selected_class="all", design=design)
 
 
 @app.route('/menu')
@@ -49,7 +51,7 @@ def menu():
     #  check if logged in
     if not session.get('logged_in'):
         return redirect('/')
-    return render_template("menu.html")
+    return render_template("menu.html", design=design)
 
 
 @app.post('/login')
@@ -71,30 +73,31 @@ def login_post():
             session['logged_in'] = True
             session['username'] = username
             session['password'] = password
+            session['roles'] = teacher_login['roles']
             return redirect(url_for('menu'))
     return redirect('/')
 
 
 @app.get('/class_select')
 def class_select():
-    #  check if logged in
-    if not session.get('logged_in'):
+    #  check if logged in and has teacher role
+    if not session.get('logged_in') or 'teacher' not in session.get('roles'):
         return redirect('/')
     # return students in selected class
     all_students = get_all_students()
     selected_class = request.args.get("class")
     students_in_class = []
     for student in all_students:
-        if student['class'] == selected_class:
+        if student['grade'] == selected_class:
             students_in_class.append(student)
     if selected_class == "all":
         return redirect(url_for('teacher'))
-    return render_template("teacher.html", students=students_in_class, selected_class=selected_class)
+    return render_template("teacher.html", students=students_in_class, selected_class=selected_class, design=design)
 
 
 @app.get('/update_call_status/<student_id>')
 def update_call_status(student_id=None):
-    #  check if logged in
+    #  check if logged in and has admin role
     if not session.get('logged_in'):
         return redirect('/')
     # update call status for student
@@ -104,15 +107,19 @@ def update_call_status(student_id=None):
             student['call_status'] = "1"
     with open('students.json', 'w') as f:
         json.dump(all_students, f, indent=4)
-    return redirect(url_for('caller'))
+    # check role and redirect to correct page
+    if 'caller' in session.get('roles'):
+        return redirect(url_for('caller'))
+    elif 'admin' in session.get('roles'):
+        return redirect(url_for('admin'))
 
 
 @app.route('/caller')
 def caller():
-    #  check if logged in
-    if not session.get('logged_in'):
+    #  check if logged in and has teacher role
+    if not session.get('logged_in') or 'caller' not in session.get('roles'):
         return redirect('/')
-    return render_template('caller.html')
+    return render_template('caller.html', design=design)
 
 
 @app.post('/search')
@@ -126,36 +133,70 @@ def search():
         if fuzz.ratio(search_term, full_name) > 20 or fuzz.ratio(search_term, student['number_plate']) > 20:
             students.append(student)
     students.sort(key=lambda x: fuzz.ratio(search_term, x['first_name'] + " " + x['last_name']), reverse=True)
-    return render_template("caller.html", search_results=students)
+    return render_template("caller.html", search_results=students, design=design)
 
 
 @app.route('/add_teacher')
 def add_teacher():
-    return render_template('add_teacher.html')
+    #  check if logged in and has admin role
+    # if not session.get('logged_in') or 'admin' not in session.get('roles'):
+    #     return redirect('/')
+    # Read teachers from JSON file
+    with open('teachers.json') as f:
+        teachers = json.load(f)
+    return render_template('add_teacher.html', teachers=teachers, design=design)
 
 
 @app.post('/add_teacher')
 def add_teacher_post():
-    # read login data from JSON file
+    #  check if logged in and has admin role
+    # if not session.get('logged_in') or 'admin' not in session.get('roles'):
+    #     return redirect('/')
+    # Read teachers from JSON file
     with open('teachers.json') as f:
-        login = json.load(f)
-    # read login data from form
+        teachers = json.load(f)
+    # read teacher data from form
     username = request.form.get("username")
     password = request.form.get("password")
-    # store login data in session
-    # encode password
+    roles = []
+    if request.form.get("admin"):
+        roles.append("admin")
+    if request.form.get("teacher"):
+        roles.append("teacher")
+    if request.form.get("caller"):
+        roles.append("caller")
+    # Hash password
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
-
-    # save new teacher to JSON file
-    new_teacher = {
-        "username": username,
-        "password": hashed_password
-    }
-    login.append(new_teacher)
+    # add teacher to list of teachers
+    teachers.append({"username": username, "password": hashed_password, "roles": roles})
+    # write teachers to JSON file
     with open('teachers.json', 'w') as f:
-        json.dump(login, f, indent=4)
+        json.dump(teachers, f, indent=4)
     return redirect(url_for('add_teacher'))
 
 
+@app.get('/reset_call_status')
+def reset_call_status():
+    #  check if logged in and has admin role
+    if not session.get('logged_in') or 'admin' not in session.get('roles'):
+        return redirect('/')
+    # reset call status for all students
+    all_students = get_all_students()
+    for student in all_students:
+        student['call_status'] = "0"
+    with open('students.json', 'w') as f:
+        json.dump(all_students, f, indent=4)
+    return redirect(url_for('admin'))
+
+
+@app.get('/logout')
+def logout():
+    session['logged_in'] = False
+    session['username'] = ""
+    session['password'] = ""
+    session['roles'] = []
+    return redirect('/')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True, use_reloader=False)
